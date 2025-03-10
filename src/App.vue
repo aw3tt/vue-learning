01edb70d49b37e577b28b14ca69162aac7ca3f37
Ivan <sir.sibiliv@gmail.com>
2025-03-10 13:31:57 +0300

    Объекты в Vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 export default {
   data() {
     return {
-      arr: [1, 2, 3],
+      obj: { x: 1, y: 2, z: 3 },
     }
   },
 }
 </script>
 
 <template>
-  <p>{{ arr[0] + arr[1] + arr[2] }}</p>
+  <p>{{ obj.x + obj.y + obj.z }}</p>
 </template>
