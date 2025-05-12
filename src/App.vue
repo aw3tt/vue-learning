8e141dfacf65c80c2a0f7d512b7509b28c946fa9
Ivan <sir.sibiliv@gmail.com>
2025-05-13 00:22:09 +0300

    Директива v-else-if

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 export default {
   data() {
     return {
-      day: 1
+      age: 20
     }
   },
   methods: {
-
+    getAgeCategory() {
+      if (this.age < 18) return 'подросток';
+      if (this.age <= 25) return 'молодой человек';
+      return 'мужчина';
+    }
   },
   computed: {
 
@@ -16,13 +20,9 @@ export default {
 
 <template>
   <div>
-    <p v-if="day === 0">Воскресенье</p>
-    <p v-if="day === 1">Понедельник</p>
-    <p v-if="day === 2">Вторник</p>
-    <p v-if="day === 3">Среда</p>
-    <p v-if="day === 4">Четверг</p>
-    <p v-if="day === 5">Пятница</p>
-    <p v-if="day === 6">Суббота</p>
+    <p v-if="age < 18">подросток</p>
+    <p v-else-if="age <= 25">молодой человек</p>
+    <p v-else>мужчина</p>
   </div>
 
 </template>
