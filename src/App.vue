4735ead94311c76a18ce5758019aeafa45fc51d3
Ivan <sir.sibiliv@gmail.com>
2025-05-13 00:11:31 +0300

    Смена текста при тогглинге

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,9 @@ export default {
 
 <template>
   <div>
-    <button @click="toggle(1)">Toggle 1</button>
-    <button @click="toggle(2)">Toggle 2</button>
-    <button @click="toggle(3)">Toggle 3</button>
+    <button @click="toggle(1)">{{ visible[1] ? 'Показать 1' : 'Скрыть 1' }}</button>
+    <button @click="toggle(2)">{{ visible[2] ? 'Показать 2' : 'Скрыть 2' }}</button>
+    <button @click="toggle(3)">{{ visible[3] ? 'Показать 3' : 'Скрыть 3' }}</button>
 
     <p v-if="visible[1]">Абзац 1</p>
     <p v-if="visible[2]">Абзац 2</p>
