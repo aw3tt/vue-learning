97b4a7c77b0da4e4ee15567013e85b91c7ab0c01
Ivan <sir.sibiliv@gmail.com>
2025-05-13 00:18:43 +0300

    Сложные условия

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      isAdmin: true
+      day: 1
     }
   },
   methods: {
@@ -16,8 +16,13 @@ export default {
 
 <template>
   <div>
-    <p v-if="isAdmin">+++</p>
-    <p v-else>---</p>
+    <p v-if="day === 0">Воскресенье</p>
+    <p v-if="day === 1">Понедельник</p>
+    <p v-if="day === 2">Вторник</p>
+    <p v-if="day === 3">Среда</p>
+    <p v-if="day === 4">Четверг</p>
+    <p v-if="day === 5">Пятница</p>
+    <p v-if="day === 6">Суббота</p>
   </div>
 
 </template>
