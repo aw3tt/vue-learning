f80b7bf16f4f25190fd3eafd828e3b8d9abcfd74
Ivan <sir.sibiliv@gmail.com>
2025-05-13 00:14:08 +0300

    Директива v-else

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,11 @@
 export default {
   data() {
     return {
-      visible: {
-        1: true,
-        2: true,
-        3: true
-      }
+      isAdmin: true
     }
   },
   methods: {
-    toggle: function(num) {
-      this.visible[num] = !this.visible[num]
-    },
+
   },
   computed: {
 
@@ -22,13 +16,8 @@ export default {
 
 <template>
   <div>
-    <button @click="toggle(1)">{{ visible[1] ? 'Показать 1' : 'Скрыть 1' }}</button>
-    <button @click="toggle(2)">{{ visible[2] ? 'Показать 2' : 'Скрыть 2' }}</button>
-    <button @click="toggle(3)">{{ visible[3] ? 'Показать 3' : 'Скрыть 3' }}</button>
-
-    <p v-if="visible[1]">Абзац 1</p>
-    <p v-if="visible[2]">Абзац 2</p>
-    <p v-if="visible[3]">Абзац 3</p>
+    <p v-if="isAdmin">+++</p>
+    <p v-else>---</p>
   </div>
 
 </template>
