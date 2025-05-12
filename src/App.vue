b70c1c313e170fa8fd0011e739b1dea06bfb1458
Ivan <sir.sibiliv@gmail.com>
2025-05-12 20:43:45 +0300

    Вспомогательные методы

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,15 @@ export default {
   },
   methods: {
     show: function() {
-      alert(this.num1 + this.num2);
-    }
+      alert(this.getDayName(3));
+    },
+    getDayName: function (dayNumber) {
+      const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+      if (dayNumber < 0 || dayNumber > 6) {
+        return 'Неверное число! Допустимо: 0-6';
+      }
+      return days[dayNumber];
+    },
   }
 }
 </script>
