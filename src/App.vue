1324d652570f87ed29a5510e5a0ee3a6bd84f301
Ivan <sir.sibiliv@gmail.com>
2025-05-14 20:35:31 +0300

    Перебор объектов

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 export default {
   data() {
     return {
-      arr: ['x', 'y', 'z'],
+    obj: {
+          user1: '100$',
+          user2: '200$',
+          user3: '300$',
+    }
     }
   },
   methods: {
@@ -16,6 +20,15 @@ export default {
 
 <template>
   <ul>
-    <li v-for="(el, key) in arr">{{key}}</li>
+    <li v-for="(el) in obj">{{el}}</li>
+  </ul>
+  <ul>
+    <li v-for="(el, key) in obj">{{key}} - {{el}}</li>
+  </ul>
+  <ul>
+    <li v-for="(el, key, index) in obj">{{key}} - {{el}} - {{index}}</li>
+  </ul>
+  <ul>
+    <li v-for="(el, key, index) in obj">{{key}} - {{el}} - {{index + 1}}</li>
   </ul>
 </template>
