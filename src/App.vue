5e35fb8c3dfe0f1096fd698dc8d94f034663167c
Ivan <sir.sibiliv@gmail.com>
2025-05-15 00:35:12 +0300

    Форма для добавления элементов массива

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,13 @@
 export default {
   data() {
     return {
-      inputText: '',
-      displayText: '',
-      showText: false,
-      clickMessage: '',
+      newItem: '',
+      items: ['a', 'b', 'c', 'd', 'e'],
     }
   },
   methods: {
-    showText2() {
-      this.displayText = this.inputText;
-      this.inputText = '';
-    },
-
-    handleClick(event) {
-      if (event.ctrlKey) {
-        this.showText = true;
-      } else {
-        this.showText = false;
-      }
-    },
-    handleLeftClick(event) {
-      this.clickMessage = 'left';
-    },
-
-    handleMiddleClick() {
-      this.clickMessage = 'middle';
-    },
-
-    handleRightClick() {
-      this.clickMessage = 'right';
-      return false; // Предотвращаем контекстное меню
+    addItem: function() {
+      this.items.unshift(this.newItem)
     }
   }
 }
@@ -39,26 +16,13 @@ export default {
 
 <template>
   <div>
-    <input
-        type="text"
-        v-model="inputText"
-        @keyup.enter="showText2"
-    >
-    <p v-if="displayText">Вы ввели: <strong>{{ displayText }}</strong></p><br/>
-
-    <a href="#" @click.prevent="handleClick">Кликните меня с зажатым Ctrl</a>
-    <p v-if="showText">Вы нажали ссылку с зажатым Ctrl!</p><br/>
-
-    <a
-        href="#"
-        @click.prevent="handleLeftClick"
-        @click.middle.prevent="handleMiddleClick"
-        @contextmenu.prevent="handleRightClick"
-    >
-      Кликните меня разными кнопками мыши
-    </a>
-
-    <p v-if="clickMessage">Нажата кнопка: <strong>{{ clickMessage }}</strong></p>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <input v-model="newItem">
+    <button @click="addItem">add</button>
   </div>
 </template>
 
