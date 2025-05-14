085ebf0344136dfae724e33de7123ef6a49e8ba6
Ivan <sir.sibiliv@gmail.com>
2025-05-14 22:11:26 +0300

    Реактивность объекта с CSS классами

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,20 @@ export default {
   data() {
     return {
       obj: {
-        done: true,
-        selected:  true,
+        hidden: true,
       },
     }
   },
   methods: {
-
+    show: function () {
+      this.obj.hidden = false
+    },
+    hide: function () {
+      this.obj.hidden = true
+    },
+    toggle: function () {
+      this.obj.hidden = !this.obj.hidden
+    }
   },
   computed: {
 
@@ -18,9 +25,14 @@ export default {
 </script>
 
 <template>
-  <input :class="obj" type="text">
+  <p :class="obj">Some text</p>
+  <button @click="show">Show text</button>
+  <button @click="hide">Hide text</button>
+  <button @click="toggle">Toggle text</button>
 </template>
 
 <style>
-
+  p.hidden {
+    display: none;
+  }
 </style>
