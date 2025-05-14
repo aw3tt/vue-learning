86c318af89420801472031852f3ea01085250a0d
Ivan <sir.sibiliv@gmail.com>
2025-05-14 21:58:17 +0300

    Стилизация компонента в фреймворке

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,10 @@
 export default {
   data() {
     return {
-      arr: ['b', 'c', 'a'],
+
     }
   },
   methods: {
-    add: function() {
-      const dateNow = new Date();
-      this.arr.push(dateNow);
-    },
-    remove_first_value: function() {
-      this.arr.splice(0, 1)
-    },
-    remove_last_value: function() {
-      this.arr.pop()
-    },
-    remove_prelast_value: function() {
-      this.arr.splice(-2, 1)
-    },
-    sort_desc: function() {
-      this.arr.sort()
-    },
-    sort_asc: function() {
-      this.arr.sort((a, b) => b.localeCompare(a))
-    }
 
   },
   computed: {
@@ -34,19 +15,16 @@ export default {
 </script>
 
 <template>
-<!--  №1-->
-  <ul v-for="item in arr">
-    <li>{{item}}</li>
-  </ul>
-  <button @click="add">add</button>
-  <!--  №2-->
-  <button @click="remove_first_value">remove first</button>
-  <!--  №3-->
-  <button @click="remove_last_value">remove last</button>
-  <!--  №4-->
-  <button @click="remove_prelast_value">remove prelast</button>
-  <!--  №5-->
-  <button @click="sort_desc">sort_desc</button>
-  <!--  №6-->
-  <button @click="sort_asc">sort_asc</button>
+  <p>
+    1
+  </p>
+  <p>
+    2
+  </p>
 </template>
+
+<style>
+  p {
+    color: purple;
+  }
+</style>
