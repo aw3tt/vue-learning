3229c8482da398d6c1affae2db6dd0aaf7bd3d35
Ivan <sir.sibiliv@gmail.com>
2025-05-14 21:35:33 +0300

    Реактивность массивов

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,29 @@
 export default {
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: 'product1',
-        },
-        {
-          id: 2,
-          name: 'product2',
-        },
-        {
-          id: 3,
-          name: 'product3',
-        },
-      ]
+      arr: ['b', 'c', 'a'],
     }
   },
   methods: {
+    add: function() {
+      const dateNow = new Date();
+      this.arr.push(dateNow);
+    },
+    remove_first_value: function() {
+      this.arr.splice(0, 1)
+    },
+    remove_last_value: function() {
+      this.arr.pop()
+    },
+    remove_prelast_value: function() {
+      this.arr.splice(-2, 1)
+    },
+    sort_desc: function() {
+      this.arr.sort()
+    },
+    sort_asc: function() {
+      this.arr.sort((a, b) => b.localeCompare(a))
+    }
 
   },
   computed: {
@@ -28,7 +34,19 @@ export default {
 </script>
 
 <template>
-  <ul v-for="product in products" :key="product.id">
-    <li>{{product.name}}</li>
+<!--  №1-->
+  <ul v-for="item in arr">
+    <li>{{item}}</li>
   </ul>
+  <button @click="add">add</button>
+  <!--  №2-->
+  <button @click="remove_first_value">remove first</button>
+  <!--  №3-->
+  <button @click="remove_last_value">remove last</button>
+  <!--  №4-->
+  <button @click="remove_prelast_value">remove prelast</button>
+  <!--  №5-->
+  <button @click="sort_desc">sort_desc</button>
+  <!--  №6-->
+  <button @click="sort_asc">sort_asc</button>
 </template>
