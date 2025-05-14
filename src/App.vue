b710d461c6c26b89bc8ed3ac1b0ba5b27868f3ba
Ivan <sir.sibiliv@gmail.com>
2025-05-14 22:13:50 +0300

    Инлайн объект с CSS классами

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,11 @@
 export default {
   data() {
     return {
-      obj: {
-        hidden: true,
-      },
+
     }
   },
   methods: {
-    show: function () {
-      this.obj.hidden = false
-    },
-    hide: function () {
-      this.obj.hidden = true
-    },
-    toggle: function () {
-      this.obj.hidden = !this.obj.hidden
-    }
+
   },
   computed: {
 
@@ -25,14 +15,11 @@ export default {
 </script>
 
 <template>
-  <p :class="obj">Some text</p>
-  <button @click="show">Show text</button>
-  <button @click="hide">Hide text</button>
-  <button @click="toggle">Toggle text</button>
+  <p :class="{active: true, valid: false}">Some text</p>
 </template>
 
 <style>
-  p.hidden {
-    display: none;
+  p.active {
+    background: yellow;
   }
 </style>
